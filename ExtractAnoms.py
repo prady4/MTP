b9981dbd6089e43725df3910b4a0ed9bf73ab20f
Pradeep <pradyrocks09@gmail.com>
2017-02-15 22:21:45 +0530

    Mid Term

--- ExtractAnoms.py
+++ ExtractAnoms.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from sklearn.decomposition import FastICA, PCA
 
-retail = pd.read_csv("processed_retail.csv")
+#
 
 def analyze_diff(center_index):
+	retail = pd.read_csv("processed_retail.csv")
 	indices = [1,2,3,4,5]
 	tmp = retail["DELHI"]*0
 	indices = list(set(indices) - {center_index})
@@ -34,9 +35,9 @@ def MovingWndw(residual, factor, days):
 	'''
 	return anomList
 
-def plot(index, days):
+def plot(index, factor, days):
 	residual = analyze_diff(index)
-	anomList = MovingWndw(residual, 1, days)
+	anomList = MovingWndw(residual, factor, days)
 	for i in anomList:
 		plt.axvline(x=i)
 	plt.plot(residual)
@@ -44,6 +45,8 @@ def plot(index, days):
 	plt.show()
 	return
 
+for i in range(1,6):
+	plot(i, 1.25, 10)
 '''
 index for all the centres are as Follows:
 1: BHUBANESHWAR
@@ -75,6 +78,7 @@ def windows(anomList, wndwSize):
 	return {'start': s, 'end': e}
 
 def ICA(index, wndwSizeMA, factor, wndwSize):
+	centre_name = ["BHUBANESHWAR", "DELHI", "LUCKNOW", "MUMBAI", "PATNA"]
 	retail = pd.read_csv("processed_retail.csv")
 	X = retail.ix[:,1:6]
 	ica = FastICA(n_components=2)
@@ -96,27 +100,53 @@ def ICA(index, wndwSizeMA, factor, wndwSize):
 	print("Number of anoms naturally: "+str(len(anomList1)))
 	x = list(set(anomList1) - set(anomList))
 	print(len(x))
+	df[3] = df[0] - ica.mean_[index-1]
+	labels = ['2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']
+	d = [i*365 for i in range(1,10)]
 	'''
+	Plot contribution of individual anomaly
+	plt.clf()
+	plt.figure(figsize=(1900/100, 1000/100), dpi=100)
+	plt.plot(retail.ix[:,index], 'r', label=centre_name[index-1])
+	plt.plot(S_.T[0]*A_[index-1][0] + ica.mean_[index-1]/2, 'c', label='IC1')
+	plt.plot(S_.T[1]*A_[index-1][1] + ica.mean_[index-1]/2, 'y', label='IC2')
+	plt.title('Contribution of ICs for '+str(centre_name[index-1]))
+	plt.xlabel("Days")
+	plt.ylabel("Prices")
+	plt.legend(loc='best')
+	plt.xticks(d, labels, rotation='vertical')
+	plt.grid()
+	plt.savefig('Contribution of ICs for '+str(centre_name[index-1]))
+	'''
+	'''
+	#Plot points reported as anomaly
 	for i in anomList:
 		plt.axvline(x=i, color='r')
 	for i in anomList1:
 		plt.axvline(x=i, color='b')
 	'''
-	#configure Window Size
+	'''
+	#Create periods of anomaly
+	'''
 	x = windows(anomList, wndwSize)
 	for i in x['start']:
 		plt.axvline(x=i, color='r')
 	for i in x['end']:
 		plt.axvline(x=i, color='b')
+	#'''
 	plt.plot(retail.ix[:,index], 'g')
 	plt.plot(df[0], 'r')
+	plt.grid()
 	plt.show()
 	return
 
+'''
 #ICA(index, wndwSizeMA, factor, wndwSize(create periods))
 ICA(2, 11, 0.75, 11)
 for i in range(1,6):
 	ICA(i, 11, 0.9, 11)
+'''
+
 '''
 index for all the centres are as Follows:
 1: BHUBANESHWAR
